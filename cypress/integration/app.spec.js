cb33dd7f56be49613392a248db9877c5e0b17731
memarino92 <memarino92@gmail.com>
2022-04-07 22:30:25 -0400

    test: added test for multiple todos

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,23 @@
 describe('React TodoMVC', () => {
-  it('adds a single todo', () => {
+  const TODO_ITEM_ONE = 'Buy Milk'
+  const TODO_ITEM_TWO = 'Pay Rent'
+  const TODO_ITEM_THREE = 'Walk Dog'
+
+  beforeEach(() => {
     cy.visit('http://localhost:8888')
-    cy.get('.new-todo').type('Buy Milk{enter}')
+  })
+
+  it('adds a single todo', () => {
+    cy.get('.new-todo').type(`${TODO_ITEM_ONE}{enter}`)
     cy.get('.todo-list li').should('have.length', 1)
+
+    cy.get('.todo-list li').eq(0).find('label').should('contain', TODO_ITEM_ONE)
+  })
+
+  it('adds three todos', () => {
+    cy.get('.new-todo').type(`${TODO_ITEM_ONE}{enter}`)
+    cy.get('.new-todo').type(`${TODO_ITEM_TWO}{enter}`)
+    cy.get('.new-todo').type(`${TODO_ITEM_THREE}{enter}`)
+    cy.get('.todo-list li').should('have.length', 3)
   })
 })
